1131e193e162d78afe7639f39a058efa5b5fc60e
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2024-07-17 02:09:12 +0000

    datatype

---
+++ Datatype.js
@@ -0,0 +1,9 @@
+// their are many datatypes 
+// number 
+// BigInt
+// null 'it is satandalone value' or having empty output
+// undefined
+// String
+// Symbol 'maximum used in ract and figma to give uniqunes'
+// boolean (either true or false)
+// Object
---
+++ variable.js
@@ -0,0 +1,6 @@
+const mobileNO = 8800;
+let name = "sagar";
+name = "shiv";
+// mobileNO = 9911;  constant can't be changed
+console.log(name)
+console.table([name, mobileNO])
