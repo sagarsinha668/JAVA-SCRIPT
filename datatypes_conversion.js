16c4bd25ec97c69c05f9241535c9e21f4c9a0f82
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2024-07-22 14:59:02 +0000

    dataconverstion

---
+++ datatypes_conversion.js
@@ -0,0 +1,19 @@
+// let x = 33;
+// let conversion = String(x)
+// console.log(conversion)
+// console.log(typeof conversion)
+
+// let x = true;
+// let conversion = String(x)
+// console.log(conversion)
+// console.log(typeof conversion)
+
+// let x = " 33ab";
+// let conversion = Number(x)
+// console.log(conversion)  
+// console.log(typeof conversion)
+
+// let x = "sagar";
+// let conversion = Number(x)
+// console.log(conversion)  
+// console.log(typeof conversion)
