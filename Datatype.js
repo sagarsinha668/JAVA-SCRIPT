5b4f31d1f4658d56bdb0509e9f0393f24e34dd6e
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2024-07-22 14:57:57 +0000

    datatypes

--- Datatype.js
+++ Datatype.js
@@ -1,3 +1,6 @@
+// ECMA documentation for set standards in js 
+// "use strict" 'treat all code as newer vrsion'
+// alert(3+3) will not works bcz we using node not browser 
 // their are many datatypes 
 // number 
 // BigInt
@@ -6,4 +9,4 @@
 // String
 // Symbol 'maximum used in ract and figma to give uniqunes'
 // boolean (either true or false)
-// Object
+// Object
