c24e8eea71194432a7a6891be06c7021c39d05c9
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2024-07-24 09:40:34 +0000

    summary of datatypes

---
+++ stack&heap.js
@@ -0,0 +1,10 @@
+// sym = (Symbol("123"))
+// // console.log(sym)
+// id = Symbol("123")
+// console.log(id===sym)
+
+
+// theire is two types of datatypes based on call by vlaue and refrence 
+// 1. primitives(call by value)=>boolean, string, number , symbol,bigint, null,undefined
+// 2. non primitives(call by refrence)=> object , function , array
+
