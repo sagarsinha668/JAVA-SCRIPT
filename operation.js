5e697a7cc6e991453e42aaa899e693e672092be7
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2024-07-24 08:20:50 +0000

    operation

---
+++ operation.js
@@ -0,0 +1,17 @@
+// console.log(+true)
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2%3)
+
+// console.log(1+1+1+1)
+
+
+// console.log(2+2+'sagar')
+// console.log('2'+2+'sagar')
+// a=2
+// b=9
+// ++b
+// a++
+// console.log(a,b)
