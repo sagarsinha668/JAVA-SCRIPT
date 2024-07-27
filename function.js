b9df5e0afa2e97cd915be0530d96144bb0a486e8
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2024-07-27 08:04:19 +0000

    fun

---
+++ function.js
@@ -0,0 +1,9 @@
+// anything written after . is function i.e .log , .push and function is called by prenthises ()
+const a = 'sagar'
+console.log(a)
+function sum(b,c){
+    const a = b+c
+    
+    console.log(a)
+}
+sum(6,7)
