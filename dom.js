9666db37a3cdd8b0415d2041863311d49d41da90
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2024-08-08 04:27:32 +0000

    1hour

---
+++ dom.js
@@ -0,0 +1,6 @@
+// window is a object that is global and created by browers , we don't define it 
+//  window.console.log("hello")
+// use window.document()
+// use console.dir(window.document)  'dir used to print object'
+
+
